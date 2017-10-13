2272fd253d9eaabea70625e1c887b8afb58e88c8
techwav <parvathikonda01@gmail.com>
2017-10-13 11:59:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-
+//store the to address in a simple variable
+$E_Addr=$_GET['mail'];
+$To_Addr = "rc239898@dal.ca";
+$subject = $_GET['subject'];
+$message = $_GET['message'];
+$name = $_GET['name'];
+$headers = <<<_END
+From: {$name} <{$E_Addr}>
+Reply-To: {$To_Addr}
+_END;
+//send an email to the recipient
+mail($To_Addr, $subject, $message, $headers);
+//redirect to thank you page
+header("Location: thanks.php");
 ?>
